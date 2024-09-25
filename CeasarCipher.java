4798a5f51bb147ee6fa8394ed0e4a8c40123167e
Max <maxchristiansoerensen@gmail.com>
2024-09-25 11:02:19 +0200

    Ceasar Cipher

---
+++ CeasarCipher.java
@@ -0,0 +1,40 @@
+package Personal_projects.Cryptography_projects;
+
+
+
+public class CeasarCipher {
+    public char[] arrayOfSmolLetters = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+    public char[] arrayOfBigLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+    String description = "The Caesar cipher... this particular cipher will ignore special characters like '!', '{}', ' ' and '#' as well as numbers";
+
+    public String getDescription(){
+        return description;
+    }
+
+    // Decryption can be done by appying the shift negative instead. If sum of index - shift < 0, write -26 + index + shift mod 26
+
+    public String shiftCode(String code, int shift){
+        char[] codeArray = code.toCharArray();
+        String encryptedCode = "";
+        for(int i = 0; i < codeArray.length; i++){ 
+            if(Character.isUpperCase(codeArray[i])) {
+                for(int j = 0; j < arrayOfBigLetters.length; j++){
+                    if(codeArray[i] == arrayOfBigLetters[j]){
+                        encryptedCode += arrayOfBigLetters[(j+shift) % 26];
+                    }
+                }
+            } else if(Character.isLowerCase(codeArray[i])){
+                for(int j = 0; j < arrayOfSmolLetters.length; j++){
+                    if(codeArray[i] == arrayOfSmolLetters[j]){
+                        encryptedCode += arrayOfSmolLetters[(j+shift) % 26];
+                    }
+                }
+            } else {
+                encryptedCode += codeArray[i];
+            }
+        }
+        return encryptedCode;
+    }
+
+
+}
---
+++ Request.java
@@ -0,0 +1,48 @@
+package Personal_projects.Cryptography_projects;
+import java.util.Scanner;
+
+public class Request {
+    String code;
+    int caesarShift;
+    public Request(){
+
+    }
+
+    // Check format of the answer, any problems in terms of unicode, the type etc. so it can be formatted for the cipher or to return an error message
+    /* 
+    public boolean validateCode(String code){
+        
+        return true;
+    }
+    */
+    CeasarCipher ceasarCipher = new CeasarCipher();
+
+    public String CaesarEncrypt(String code, int shift){
+        return ceasarCipher.shiftCode(code, shift);
+    }
+
+    public void greeting(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello! What would you like to encrypt?: ");
+        String code = sc.nextLine();
+        System.out.println("Your encryption options are:");
+        System.out.println("1: Ceasar cipher");
+        System.out.println("2: Vigenere cipher");
+        System.out.println("For detailed information about each cipher, enter 0: ");
+        System.out.println("Please enter a number: ");
+        int input = sc.nextInt();
+        if(input == 1){
+            System.out.println("Please enter the number you want to shift the code with: ");
+            int shift = sc.nextInt();
+            System.out.println(CaesarEncrypt(code, shift));
+        } else if(input == 2){
+            System.out.println("Please enter the key you want to shift the code with: ");
+            String shift = sc.nextLine();
+            //System.out.println(VigenereEncrypt(code, shift));
+        } else if(input == 0){
+            ceasarCipher.getDescription();
+        }
+        sc.close();
+    }
+
+}
